eed8f2eda555be8945b955650cd216a2c56816cd
Floxion <mazentamer462@gmail.com>
2026-08-18 00:42:59 +0300

    Update economy.js

--- src/utils/economy.js
+++ src/utils/economy.js
@@ -16,9 +16,9 @@ const WORK_MIN = ECONOMY_CONFIG.workMin || 10;
 const WORK_MAX = ECONOMY_CONFIG.workMax || 100;
 const COOLDOWNS = ECONOMY_CONFIG.cooldowns || {
 daily: 24 * 60 * 60 * 1000,
-work: 60 * 60 * 1000,
-crime: 2 * 60 * 60 * 1000,
-rob: 4 * 60 * 60 * 1000,
+work: 30 * 60 * 1000,
+crime: 30 * 60 * 1000,
+rob: 1 * 60 * 60 * 1000,
 };
 
 export function getEconomyKey(guildId, userId) {
